6bad713704c3985b2c0639436d19ed1da9c7c82a
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2022-09-05 16:48:40 -0400

    update mint

--- src/utils/web3.tsx
+++ src/utils/web3.tsx
@@ -4,10 +4,20 @@ declare let window: any;
 // import contractABI from "../contracts/proxy.json";
 // const contractAddress = "0x72A40770537525c4E67FE8121409100DCf141718";
 import contractABI from "../contracts/tagnft.json";
-const contractAddress = "0x488Ab03ce728429dcB64c31CB9Df9390F61DC21e";
-const TAG = 1;
+const contractAddress = "0xCF8E091466cF4F0A9c734793BD7ede9B9761FD12";
 
-let provider: any, smartContract, signer: Signer | undefined, contractWithSigner: Contract, address: string;
+const wethContractAddress = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+import wethContractABI from "../contracts/weth.json";
+
+const TAG = 1;
+const BUY_PRICE = 0.0102;
+
+let provider: any,
+  smartContract,
+  signer: Signer | undefined,
+  contractWithSigner: Contract,
+  address: string,
+  wethContractWithSigner: Contract;
 
 let has_error = false;
 
@@ -16,21 +26,30 @@ const metamaskInstalled = () => {
   return false;
 };
 
-const initConnection = async() => {
-  const provider =  metamaskInstalled()? new ethers.providers.Web3Provider(window.ethereum): undefined;
+const initConnection = async () => {
+  const provider = metamaskInstalled()
+    ? new ethers.providers.Web3Provider(window.ethereum)
+    : undefined;
   const smartContract = new ethers.Contract(
-  contractAddress,
-  contractABI,
-  provider
-);
+    contractAddress,
+    contractABI,
+    provider
+  );
+
+  const wethContract = new ethers.Contract(
+    wethContractAddress,
+    wethContractABI,
+    provider
+  );
 
   signer = provider?.getSigner();
   if (signer) {
     contractWithSigner = smartContract.connect(signer);
+    wethContractWithSigner = wethContract.connect(signer);
     address = await signer?.getAddress();
   }
-  if (!contractWithSigner || !address) 
-    has_error = true;
+
+  if (!contractWithSigner || !address) has_error = true;
   else has_error = false;
 };
 
@@ -58,15 +77,19 @@ const sellTagNft = async (count: number) => {
 
 const buyTagNft = async (count: number) => {
   let rlt = false;
-  await contractWithSigner?.buyTag(count).then(
-    async (trxHash: any) => {
-      await trxHash.wait(1);
-      rlt = true;
-    },
-    (error: unknown) => {
-      console.log(error);
-    }
-  );
+  await wethContractWithSigner
+    ?.approve(contractAddress, ethers.utils.parseEther(BUY_PRICE.toString()))
+    .then(async () => {
+      await contractWithSigner?.buyTag(count).then(
+        async (trxHash: any) => {
+          await trxHash.wait(1);
+          rlt = true;
+        },
+        (error: unknown) => {
+          console.log(error);
+        }
+      );
+    });
   return rlt;
 };
 
@@ -84,10 +107,4 @@ const mintTagNft = async (count: number) => {
   return rlt;
 };
 
-export {
-  initConnection,
-  getTagCount,
-  sellTagNft,
-  buyTagNft,
-  mintTagNft
-};
+export { initConnection, getTagCount, sellTagNft, buyTagNft, mintTagNft };
